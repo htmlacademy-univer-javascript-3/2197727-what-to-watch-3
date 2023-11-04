f4180c3c4e34732e3e4da76a8a7a7aeade1bc20a
Serkik <63666583+Serckik@users.noreply.github.com>
2023-11-04 09:44:26 +0500

    начинаем программировать

---
+++ src/const.ts
@@ -0,0 +1,7 @@
+const Setting = {
+  PromoFilmTitle: 'The Grand Budapest Hotel',
+  PromoFilmGenre: 'Drama',
+  PromoFilmYear: 2014,
+} as const;
+
+export default Setting;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app';
+import Setting from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,5 +10,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <h1>Hello, World!</h1>
+    <App
+      promoFilmTitle={Setting.PromoFilmTitle}
+      promoFilmGenre={Setting.PromoFilmGenre}
+      promoFilmYear={Setting.PromoFilmYear}
+    />
   </React.StrictMode>
 );
